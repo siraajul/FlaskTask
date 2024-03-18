bd4e6d602a2dcf8a3241ea0381d7879d06e7c55b
siraajul <shirajulislamparvez@gmail.com>
2024-03-19 03:01:32 +0600

    --update [DB Name]

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ class Flasktask(db.Model):
 
 @app.route('/')
 def hello_world():
-    flasktasks = Flasktask(title="FlaskTask", desc="Add Our FirstTask", date_created=datetime)
+    flasktask = Flasktask(title="FlaskTask", desc="Add Our FirstTask", date_created=datetime)
+    db.session.add(flasktask)
+    db.session.commit()
     return render_template('index.html')
 
 
