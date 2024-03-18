f693448de4c43fc43f530c6614372834dbc679f8
siraajul <shirajulislamparvez@gmail.com>
2024-03-19 02:46:05 +0600

    --add SQLAlchemy DB into App

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from _datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///flasktask.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Flasktask(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return "{self.sno} - {self.title}"
 
 
 @app.route('/')
 def hello_world():
+    flasktasks = Flasktask(title="FlaskTask", desc="Add Our FirstTask", date_created=datetime)
     return render_template('index.html')
 
 
